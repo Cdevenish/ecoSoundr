bf49277c9ceb768db6e6457c6f51fa07803b05ca
Christian Devenish <c.devenish@keele.ac.uk>
2024-04-21 08:03:37 +0100

    add helpfiles

---
+++ R/helpfiles.r
@@ -0,0 +1,20 @@
+#' @name AudioTest
+#' @title  Test and play audio in learnr package
+#' @description
+#' Test play sound
+#' @keywords ecological sounds
+#' @examples
+#' \dontrun{
+#' learnr::run_tutorial("audioTest", package = "ecoSoundr")
+#' }
+NULL
+#' @name Example_Sounds
+#' @title Example Sounds
+#' @description
+#' Example
+#' @keywords sounds
+#' @examples
+#' \dontrun{
+#' learnr::run_tutorial("example", package = "ecoSoundr")
+#' }
+NULL
